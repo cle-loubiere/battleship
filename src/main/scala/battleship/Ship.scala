c7d6439e690e46a071d6162bdbeb984dc678fe99
Pouzee <cleloubiere@gmail.com>
2018-10-03 16:45:18 +0200

    ship and shipSpec classes

--- src/main/scala/battleship/Ship.scala
+++ src/main/scala/battleship/Ship.scala
@@ -1,5 +1,45 @@
 package battleship
 
-class Ship {
-    def twice(i: Int):Int = 2*i
+case class Ship(val hitList:List[Boolean], orientation:Char, posX:Int, posY:Int ) {
+    
+    val length = hitList.length
+    val direction = {
+        if(orientation == 'n') 's'
+        else if(orientation == 'w') 'e'
+        else orientation
+    } 
+    val coordinateX = {
+        if(orientation == 'w') posX - length +1
+        else posX
+    }
+    val coordinateY ={
+        if(orientation == 'n') posY - length +1
+        else posY
+    }
+    //val hitList = List.fill(length)(false)
+   
+    def isInCoordinate(x: Int, y:Int):Boolean = {
+        if(direction == 'e'){
+            return (posX <= x && (posX+length) > x && posY == y)
+        }else{
+            return (posY <= y && (posY+length) > y && posX == x)
+        }
+    }
+
+    def isSunk():Boolean = {
+        hitList.forall(x => x==true)
+    }
+
+    def shoot(x:Int, y:Int):Ship = {
+        if(this.isInCoordinate(x,y)){
+            if((x-posX) > 0){
+                val newHitList = hitList.updated(x-posX,true)
+                this.copy(hitList = newHitList)
+            }else{
+                val newHitList = hitList.updated(y-posY,true)
+                this.copy(hitList = newHitList)
+            }
+            
+        }else this
+    }
 }
--- src/test/scala/battleship/ShipSpec.scala
+++ src/test/scala/battleship/ShipSpec.scala
@@ -2,13 +2,64 @@ package battleship
 import org.scalatest.FunSuite
 
 
-object ShipSpec extends FunSuite{
+class ShipSpec extends FunSuite{
 
-    test("pliz"){
-        assert(1==1)
+    test("constructor basic information test"){
+        val ship = new Ship(List.fill(1)(false),'s',1,1)
+        assert( ship.length == 1)
+        assert(ship.direction== 's')
+        assert( ship.coordinateX == 1)
+        assert(ship.coordinateY== 1)
     }
 
-    test("test double "){
-        assert(new Ship().twice(2) == 2*2)
+    test("constructor orientation = 'north' test"){
+        val ship = new Ship(List.fill(2)(false),'n',3,3)
+        assert(ship.direction== 's')
+        assert(ship.coordinateX == 3)
+        assert(ship.coordinateY== 2)
     }
+    
+    test("constructor orientation = 'west' test"){
+        val ship = new Ship(List.fill(2)(false),'w',3,3)
+        assert(ship.direction== 'e')
+        assert(ship.coordinateX == 2)
+        assert(ship.coordinateY== 3)
+    }
+
+    test("isInCoordinate(int,int) ship horizontal test"){
+        val ship = new Ship(List.fill(3)(false),'e',3,3)
+        assert(ship.isInCoordinate(3,3))
+        assert(ship.isInCoordinate(4,3))
+        assert(ship.isInCoordinate(5,3))
+        assert(!(ship.isInCoordinate(2,3)))
+        assert(!(ship.isInCoordinate(6,3)))
+        assert(!(ship.isInCoordinate(3,2)))
+        assert(!(ship.isInCoordinate(3,4)))
+    }
+
+    test("isInCoordinate(int,int) ship vertical test"){
+        val ship = new Ship(List.fill(3)(false),'s',3,3)
+        assert(ship.isInCoordinate(3,3))
+        assert(ship.isInCoordinate(3,4))
+        assert(ship.isInCoordinate(3,5))
+        assert(!(ship.isInCoordinate(3,2)))
+        assert(!(ship.isInCoordinate(3,6)))
+        assert(!(ship.isInCoordinate(2,3)))
+        assert(!(ship.isInCoordinate(4,3)))
+    }
+
+    test("isSunk() test"){
+        val ship = new Ship(List.fill(1)(false),'s',1,1)
+        assert(!(ship.isSunk))
+        assert(ship.shoot(1,1).isSunk)
+    }
+    test("shoot() test"){
+        val ship = new Ship(List.fill(2)(false),'s',1,1)
+        assert(!(ship.isSunk))
+        assert(!ship.shoot(1,1).isSunk)
+        assert(ship.shoot(1,1).shoot(1,2).isSunk)
+        assert(!ship.shoot(1,1).shoot(1,3).isSunk)
+        assert(!ship.shoot(1,1).shoot(1,1).isSunk)
+    }
+    
 }
